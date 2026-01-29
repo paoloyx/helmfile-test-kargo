6579a133045353a4c8a42d546f7fdafb971ae909
Paolo Filippelli <paolo.filippelli@wolterskluwer.com>
2026-01-29 16:39:50 +0100

    prod replicas to 4

---
+++ kargo.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+set -x
+
+argo_cd_chart_version=9.3.7
+argo_rollouts_chart_version=2.40.5
+cert_manager_chart_version=v1.19.2
+k8s_version=v1.34.3-k3s1
+rootdir=$(pwd -P)
+cluster_name=kargo-quickstart
+
+k3d cluster create $cluster_name \
+  --no-lb \
+  --k3s-arg '--disable=traefik@server:0' \
+  --image rancher/k3s:$k8s_version \
+   --volume /usr/local/share/ca-certificates/wk-certs/wk-zscaler-rootca.crt:/etc/ssl/certs/wk-zscaler-rootca.crt \
+  -p '31443-31445:31443-31445@servers:0:direct' \
+  -p '32080-32082:32080-32082@servers:0:direct' \
+  --wait
+
+# import images that we are not able to pull directly from k3d/k3s due to zscaler
+docker pull ecr-public.aws.com/docker/library/redis:8.2.2-alpine
+k3d images import  ecr-public.aws.com/docker/library/redis:8.2.2-alpine -c $cluster_name
+
+helm install cert-manager cert-manager \
+  --repo https://charts.jetstack.io \
+  --version $cert_manager_chart_version \
+  --namespace cert-manager \
+  --create-namespace \
+  --set crds.enabled=true \
+  --wait
+
+helm install argocd argo-cd \
+  --repo https://argoproj.github.io/argo-helm \
+  --version $argo_cd_chart_version \
+  --namespace argocd \
+  --create-namespace \
+  --values $rootdir/manifests/argocd-values.yaml \
+  --set 'configs.secret.argocdServerAdminPassword=$2a$10$5vm8wXaSdbuff0m9l21JdevzXBzJFPCi8sy6OOnpZMAG.fOXL7jvO' \
+  --set dex.enabled=false \
+  --set notifications.enabled=false \
+  --set server.service.type=NodePort \
+  --set server.service.nodePortHttp=31443 \
+  --set server.extensions.enabled=true \
+  --set 'server.extensions.contents[0].name=argo-rollouts' \
+  --set 'server.extensions.contents[0].url=https://github.com/argoproj-labs/rollout-extension/releases/download/v0.3.3/extension.tar' \
+  --wait
+
+helm install argo-rollouts argo-rollouts \
+  --repo https://argoproj.github.io/argo-helm \
+  --version $argo_rollouts_chart_version \
+  --create-namespace \
+  --namespace argo-rollouts \
+  --wait
+
+# Password is 'admin'
+helm install kargo \
+  oci://ghcr.io/akuity/kargo-charts/kargo \
+  --namespace kargo \
+  --create-namespace \
+  --set api.service.type=NodePort \
+  --set api.service.nodePort=31444 \
+  --set api.adminAccount.passwordHash='$2a$10$Zrhhie4vLz5ygtVSaif6o.qN36jgs6vjtMBdM6yrU1FOeiAAMMxOm' \
+  --set api.adminAccount.tokenSigningKey=iwishtowashmyirishwristwatch \
+  --set externalWebhooksServer.service.type=NodePort \
+  --set externalWebhooksServer.service.nodePort=31445 \
+  --wait
